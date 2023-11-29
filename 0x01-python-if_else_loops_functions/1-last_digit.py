11b66b4bbe900c061f57de91bfca0527ea28c7d5
Imrane Arabah <59772524+arabahimrane@users.noreply.github.com>
2023-11-28 19:34:03 -0500

    last correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
-
-if number < 0:
-    lastdegit = -(-number % 10)
+last = abs(number) % 10
+if (number < 0):
+    last = -(abs(number) % 10)
 else:
-    lastdegit = number % 10
-if (lastdegit > 5):
-    print(f"Last digit of {number} is {lastdegit} and is greater than 5")
-elif (lastdegit == 0):
-    print(f"Last digit of {number} is {lastdegit} and is 0")
+    last = abs(number) % 10
+if (last > 5):
+    print(f"Last digit of {number} is {last} and is greater than 5")
+elif (last == 0):
+    print(f"Last digit of {number} is 0 and is 0")
 else:
-    print(f"Last digit of {number} is {lastdegit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - insert a nbr in sorted list listint_t list
+ * @head: pointer to head of list
+ * @number: number
+ * Return: adress of nodes or NULL
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current;
+	listint_t *new;
+
+	current = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+	if (!current)
+	{
+		*head = new;
+		return (new);
+	}
+	while (current != NULL)
+	{
+		if (current->n >= new->n)
+		{
+			new->next = current;
+			*head = new;
+			return (new);
+		}
+		else if (!current->next)
+			return (add_nodeint_end(head, number));
+		else if ((current->n <= new->n && current->next->n >= new->n))
+		{
+			new->next = current->next;
+			current->next = new;
+			return (new);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
