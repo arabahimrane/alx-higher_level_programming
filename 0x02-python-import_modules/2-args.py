36cd0b948f1000e67c91e62ea9baba04e6a20c05
Imrane Arabah <59772524+arabahimrane@users.noreply.github.com>
2023-11-30 22:22:13 -0500

    projet2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+if (len(sys.argv) - 1 == 0):
+    print("0 arguments.")
+elif (len(sys.argv) - 1 == 1):
+    print("1 argument:")
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+
+for item in range(1, len(sys.argv)):
+    print("{:d}: {:s}".format(item, sys.argv[item]))
